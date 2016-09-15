8d9ba330f218d9a138a2223b0fa5c19ca78a978d
amel johny <amel.johny@gmail.com>
2016-09-15 17:02:03 +0530

    ui changes

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@
 $(function(){
 
     /* start typed element */
-    //http://stackoverflow.com/questions/24874797/select-div-title-text-and-make-array-with-jquery
+  
     var subElementArray = $.map($('.sub-element'), function(el) { return $(el).text(); });    
     $(".element").typed({
         strings: subElementArray,
